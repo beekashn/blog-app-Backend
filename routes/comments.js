912216a7cfa6ad627ed65f85f25649834385201a
beekashn <git7beekash7@gmail.com>
2024-06-06 16:36:07 +0545

    2nd commit

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/User");
 const { route } = require("./auth");
 const bcrypt = require("bcrypt");
 const Post = require("../models/post");
@@ -53,8 +53,5 @@ router.get("/post/:postId", async (req, res) => {
     res.status(500).json(error);
   }
 });
-
-
-
 
 module.exports = router;
